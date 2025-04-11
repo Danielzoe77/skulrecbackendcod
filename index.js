3c5caa7e60317751d5a25489a3ae26c6dd6b1527
ehisd <omoanondan@gmail.com>
2025-04-11 12:44:08 -0300

    updated sync1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser')
 const app = express();
 const cors = require('cors');
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://admin-dash-mauve.vercel.app/",],
+    origin: [ "https://admin-dash-mauve.vercel.app/", "http://localhost:5173"],
     credentials: true,            
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: [
