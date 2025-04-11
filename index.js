9966350c814b3ef5aa7b6caca8db2bb63911efe8
ehisd <omoanondan@gmail.com>
2025-04-11 11:52:48 -0300

    updated sync

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var cookieParser = require('cookie-parser')
 const app = express();
 const cors = require('cors');
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://weather-app-zoe.vercel.app",],
+    origin: ["http://localhost:5173", "https://admin-dash-mauve.vercel.app/",],
     credentials: true,            
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: [
